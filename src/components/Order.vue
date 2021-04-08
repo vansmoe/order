0c3f240a13a372a53de77e897fec52d8ef30f9e4
vansmoe <61562397+vansmoe@users.noreply.github.com>
2021-04-08 11:25:05 +0200

    Display improvements

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="wrapper">
-    <h3>{{ orderData.positionName }}</h3>
-    <div class="details">
-      <div class="tile">
-        <img class="icon" src="@/assets/apartment.png" alt="Apartment icon">
-        {{ orderData.clientNameShort }}</div>
-      <div class="tile">
-        <img class="icon" src="@/assets/marker.png" alt="Marker icon">
-        {{ orderData.address.city }}</div>
-      <div class="tile">
-        <img class="icon" src="@/assets/calendar.png" alt="Calendar icon">
-        {{ this.getDate(orderData.dateBegin, orderData.dateEnd) }}</div>
-      <div class="tile highlight">
-        {{ orderData.price }} PLN/hour</div>
-      <button class="tile btn-apply">Apply</button>
+  <div class="wrapper"> 
+    <div class="tile">
+      <img class="img" src="@/assets/photo.png" alt="Profile photo">
+    </div>
+    <div class="job">
+      <h3 class="job-title">{{ orderData.positionName }}</h3>
+      <div class="job-details">
+        <div class="tile job-tile">
+          <img class="icon" src="@/assets/apartment.png" alt="Apartment icon">
+          {{ orderData.clientNameShort }}</div>
+        <div class="tile job-tile">
+          <img class="icon" src="@/assets/marker.png" alt="Marker icon">
+          {{ orderData.address.city }}</div>
+        <div class="tile job-tile">
+          <img class="icon" src="@/assets/calendar.png" alt="Calendar icon">
+          {{ this.getDate(orderData.dateBegin, orderData.dateEnd) }}</div>
+      </div>
+    </div>
+    <div class="tile highlight">{{ orderData.price }} PLN/hour</div>
+    <div class="tile">
+      <button class="btn-apply">Apply</button>
     </div>
   </div>
 </template>
@@ -44,29 +50,52 @@ export default {
 $primary: #2196f3
 
 .wrapper
-  max-width: 1200px
+  max-width: 1000px
   margin: 0 auto
   padding: 20px
-  text-align: left
-
-.details
   display: flex
   flex-wrap: wrap
-  text-align: center
   justify-content: space-between
-
-  @media (max-width: 500px)
-    flex-direction: column
-
-  @media (min-width: 800px)
-    display: grid
-    grid-template-columns: repeat(4, 1fr) .5fr
+  align-items: center
 
 .tile
+  padding: 0 10px
   display: flex
   align-items: center
   justify-content: center
-  flex: 1 1 50%
+  font-size: 15px
+  flex: 1 1
+
+  @media (max-width: 800px)
+    &:last-child
+      flex: 1 1 100%
+
+
+.job
+  display: flex
+  flex-wrap: wrap
+  justify-content: space-between
+  flex: 0 0 50%
+
+.job-title
+  margin-top: 0
+  margin-left: 20px
+  font-size: 20px
+  font-weight: 100
+  text-align: left
+  flex: 1 1 100%
+
+.job-details
+  display: flex
+  flex-wrap: nowrap
+  align-content: center
+
+  @media (max-width: 800px)
+    flex-direction: column
+
+.job-tile
+  @media (max-width: 800px)
+    justify-content: left
 
 .icon
   max-height: 25px
@@ -74,10 +103,11 @@ $primary: #2196f3
 
 .highlight
   color: $primary
-  font-size: 20px
-  font-weight: 600
+  font-size: 18px
   justify-self: center
-  
+
+.img
+  max-height: 60px
 
 .btn-apply
   @include reset-button-defaults
@@ -89,7 +119,6 @@ $primary: #2196f3
   color: #fff
   font-weight: 600
   justify-self: center
-  flex: 1 1 100%
 
   &:hover
     opacity: .7
